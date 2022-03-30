35b1353e901a53136b99b0ca9e1b6c0951ea931a
maugl <maximilian.gutsche.21@um.edu.mt>
2022-03-30 10:33:02 +0200

    add extract text from chat files feature

--- twitch_scripts/extract_video_info.py
+++ twitch_scripts/extract_video_info.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from zipfile import ZipFile
 import pandas as pd
 import glob
+import datetime.datetime
 
 
 def load_vid_info(info_files_path):
@@ -41,6 +42,28 @@ def load_vids_chat(chat_files_path, vid_info, vid2ind):
     return vid_info
 
 
+def extract_messages(chat_files_path, out_dir, vid_info, vid2ind):
+    chat_files = glob.glob(chat_files_path)
+    num_files_extracted = 0
+    for chat_file_name in chat_files:
+        zf = ZipFile(chat_file_name)
+        for f_name in zf.namelist():            
+            if f_name.endswith("json"):
+                with zf.open(f_name, "r") as in_file:
+                    vid_chat = json.load(in_file)
+                    video_id = f_name.split("/")[-1].strip(".json")
+                    with open(f"{out_dir}/{video_id}.txt", "w") as out_file:
+                        out_file.write("\n".join([msg["message"]["body"] for msg in vid_chat["comments"]]))
+                    num_files_extracted += 1
+            if i%25 == 0:
+                print(f"{datetime.now().strftime('%Y/%m/%d_%H:%M:%S')}: extracted {num_files_extracted} chat files.")
+                        
+
+def make_corpus_dir(corpus_dir):
+    if not os.path.exists(corpus_dir):
+        os.makedirs(corpus_dir)
+
+
 def save_vid_info(vid_info, vid_info_out):
     cols = ["id", "title", "created_at", "msg_count", "is_rerun", "duration"]
 
@@ -52,15 +75,25 @@ def save_vid_info(vid_info, vid_info_out):
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="load information from downloaded video chat  zip files")
-    parser.add_argument("info_files_path", help="path where to load the info about the videos from")
-    parser.add_argument("chat_files_path", help="path where to load chat data from")
-    parser.add_argument("output", help="path where to save the extracted data")
+    parser.add_argument("-i", "info_files_path", help="path where to load the info about the videos from")
+    parser.add_argument("-c", "chat_files_path", help="path where to load chat data from")
+    parser.add_argument("-o", "output", help="path where to save the extracted data")
+    parser.add_argument("-m", "mode", choices=["vid_info", "chat_messages"], help="Whch action to perform, if 'vid_info', a csv file with message counts and video information is created. If 'chat_messages', all the messages are extracted into text files, one message per line, one file per video and written.")
 
-    # example run:
-    # python3 extract_video_info "/home/mgut1/data/video_info/*_vids.json" "/home/mgut1/data/videos_chat/*_vids_chat.zip" /home/mgut1/data/video_info/vid_info.csv
+    # example run (chat messages):
+    # python3 extract_video_info -i "/home/mgut1/data/video_info/*_vids.json" -c "/home/mgut1/data/videos_chat/*_vids_chat.zip" -o /home/mgut1/data/videos_chat/corpus -m chat_messages
+    # test run (chat messages):
+    # python3 extract_video_info -i "/home/mgut1/data/video_info/*_vids.json" -c "/home/mgut1/data/videos_chat/*_vids_chat.zip" -o /home/mgut1/data/videos_chat/corpus -m chat_messages
 
     args = parser.parse_args()
-
+    
+    # prepare general video information
     v_inf, v2i = load_vid_info(args.info_files_path)
-    v_inf = load_vids_chat(args.chat_files_path, v_inf, v2i)
-    save_vid_info(v_inf, args.output)
+    
+    if args.mode == "vid_info":
+        v_inf = load_vids_chat(args.chat_files_path, v_inf, v2i)
+        save_vid_info(v_inf, args.output)
+    if args.mode == "chat_messages":
+        print(f"{datetime.now().strftime('%Y/%m/%d_%H:%M:%S')}: extracting chat messages...")
+        make_corpus_dir(args.output)
+        extract_messages("../data/videos_chat/*_vids_chat.zip", args.output, vinf, v2i)
