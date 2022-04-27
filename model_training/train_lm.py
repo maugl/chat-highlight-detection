3af2f038e1ac93e206a0c26a23867068c60c4a75
maugl <maximilian.gutsche.21@um.edu.mt>
2022-04-27 10:17:03 +0200

    improved logging

--- model_training/train_lm.py
+++ model_training/train_lm.py
@@ -1,4 +1,6 @@
+import argparse
 from os.path import exists
+import json
 
 import torch
 
@@ -16,16 +18,19 @@ from datasets import load_dataset
 class CannotLoadTokenizer(Exception):
     pass
 
+
 def train_tokenizer(ds, out_file_path):
     batch_size = 1000
 
     def batch_iterator():
         for i in range(0, len(ds["train"]), batch_size):
-            yield ds["train"][i: i + ds]["text"]
+            yield ds["train"][i: i + batch_size]["text"]
 
+    print("training tokenizer...")
     tokenizer_old = RobertaTokenizerFast.from_pretrained("roberta-base")
     tokenizer_new = tokenizer_old.train_new_from_iterator(batch_iterator(), vocab_size=50000)
     tokenizer_new.save_pretrained(out_file_path)
+    print("training tokenizer DONE")
 
 
 def tokenize_dataset(tokenizer_files_path, ds):
@@ -34,9 +39,9 @@ def tokenize_dataset(tokenizer_files_path, ds):
     def tokenize_function(examples):
         # using return_special_tokens_mask=True for optimized DataCollator later
         return tokenizer_tok_data(examples["text"], return_special_tokens_mask=True)
-
-    dataset_tok = ds.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
-
+    print("tokenizing dataset...")
+    dataset_tok = ds.map(tokenize_function, batched=True, num_proc=2, remove_columns=["text"])
+    print("tokenizing dataset DONE")
     return dataset_tok
 
 
@@ -88,7 +93,7 @@ def train(model_train, tokenizer_train, ds, output_dir):
         per_device_train_batch_size=64,
         save_steps=10_000,
         save_total_limit=2,
-        prediction_loss_only=True,
+        prediction_loss_only=True
     )
 
     trainer = Trainer(
@@ -98,18 +103,22 @@ def train(model_train, tokenizer_train, ds, output_dir):
         train_dataset=ds['train']
     )
 
-    trainer.train(resume_from_checkpoint=True)
+    trainer.train()
 
     trainer.save_model()
 
 
-def group_dataset(ds):
-    return ds.map(
+def group_dataset(ds, tker):
+    print("grouping dataset...")
+    grouped_ds = ds.map(
         group_texts,
+        fn_kwargs={"tokenizer": tker},
         batched=True,
         batch_size=1000,
-        num_proc=4,
+        num_proc=2
     )
+    print("grouping dataset DONE")
+    return grouped_ds
 
 
 def group_texts(examples, tokenizer, block_size=128):
@@ -144,7 +153,7 @@ def group_texts(examples, tokenizer, block_size=128):
     return result
 
 
-def load_data(m_path, d_path):
+def load_data(m_path, d_path, tokenizer):
     # load dataset from disk if it has been created before
     if exists(f"{d_path.rstrip('/')}/corpus_grouped_dataset"):
         ds_lm = datasets.DatasetDict.load_from_disk(f"{d_path.rstrip('/')}/corpus_grouped_dataset")
@@ -156,33 +165,43 @@ def load_data(m_path, d_path):
             ds_text = load_huggingface_dataset(f"{d_path.rstrip('/')}/twitch_lol_combined.txt")
             dataset_tokenized = tokenize_dataset(ds=ds_text, tokenizer_files_path=m_path)
             dataset_tokenized.save_to_disk(f"{d_path.rstrip('/')}/corpus_tokenized_dataset")
-        dataset_grouped = group_dataset(dataset_tokenized)
+        dataset_grouped = group_dataset(dataset_tokenized, tokenizer)
         dataset_grouped.save_to_disk(f"{d_path.rstrip('/')}/corpus_grouped_dataset")
         ds_lm = dataset_grouped
 
     return ds_lm
 
 
-def main():
-    check_for_cuda()
+def main(arguments):
+    # check_for_cuda()
 
     model_path = "/netscratch/gutsche/data/TwitchLeagueBert"
     data_path = "/netscratch/gutsche/data/"
 
     try:
+        print("load tokenizer from disk")
         tokenizer = load_tokenizer(model_path)
     except CannotLoadTokenizer as e:
         # if there is no tokenizer, train it
+        print("load dataset from text file")
         ds_text = load_huggingface_dataset(f"{data_path.rstrip('/')}/twitch_lol_combined.txt")
         train_tokenizer(ds=ds_text, out_file_path=model_path)
         tokenizer = load_tokenizer(model_path)
 
-    dataset_lm = load_data(model_path, data_path)
+    dataset_lm = load_data(model_path, data_path, tokenizer)
 
-    # model = load_model()
+    if arguments.train_model:
+        model = load_model()
 
-    #train(model, tokenizer, dataset_lm, model_path)
+        train(model, tokenizer, dataset_lm, model_path)
 
+def log_progress(log):
+    print(json.dumps(log))
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="train a tokenizer and language model for twitch chat data")
+    parser.add_argument('--train_model', action='store_true')
+    parser.set_defaults(feature=True)
+    args = parser.parse_args()
+
+    main(args)
